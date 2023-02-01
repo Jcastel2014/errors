6f7cfd830346353bd22b3d373ba856b16bb404a0
Jcastel2014 <2020253542@ub.edu.bz>
2023-02-01 13:04:22 -0600

    yes sirr

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 // demonstrate error
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
-func area(length, width float64) float64 {
+func area(length, width float64) (float64, error) {
+
+	//check if lenght is greater than 0
+	if length < 0 {
+		err := errors.New("no negative numbers")
+		return -1, err
+	}
+
+	if width < 0 {
+		err := errors.New("no negative numbers")
+		return -1, err
+	}
+
 	result := length * width
-	return result
+
+	return result, nil
 
 }
 
 func main() {
-	length := 7
+	length := -7
 	width := -2
-	result := area(float64(length), float64(width))
+	result, err := area(float64(length), float64(width))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(result)
 }
